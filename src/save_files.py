4a3aef51e173a57133b0b2e93ca1ab136d3a6ef4
khazi <codewithrazi@gmail.com>
2022-09-03 16:39:11 +0200

    some .xml and .py files added

--- src/save_files.py
+++ src/save_files.py
@@ -1,12 +1,32 @@
 from pyspark.sql.functions import DataFrame
 import os
+import datetime as dt
+from pyspark.sql import SparkSession
 
 
-def to_csv(csv_path: str, df: DataFrame):
+def to_csv(spark: SparkSession, csv_path: str, df: DataFrame):
+	# csv_date = dt.datetime.today().strptime("%Y-%m-%d")
 	if len(os.listdir(csv_path)) == 0:
 		df.write.option("header", True)\
 				.option("delimeter", ";")\
-				.csv(csv_path+ "jobs_extract")
+				.csv(csv_path+ "jobs_extract/" + "df_final.csv")
 	else:
-		df.write.save(path=csv_path, format='csv', mode='append', sep= ';')
+		spark.read.option("header", True)\
+				.option("delimeter", ";")\
+				.csv(csv_path + "jobs_extract/" + "df_final.csv") \
+				.union(df)\
+				.coalesce(1)\
+				.write.option("header", True)\
+				.option("delimeter", ";")\
+				.mode("Overwrite")\
+				.csv(csv_path + "jobs_extract/" + "df_final.csv")
+
+
+def append_dataframe(output_path: str, df_empty: DataFrame):
+	if os.path.exists(output_path+ "df_final"):
+		pass
+		#df_final = df_final.union(df_empty)
+	else:
+		pass
+
 
